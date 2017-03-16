c229e255c1783fefc13cc5aa16c351824b9854d7
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-03-16 15:15:46 +0100

    Expected implementation!

--- FirstSolution/IntechCode/IntechCollection/IMyDictionary.cs
+++ FirstSolution/IntechCode/IntechCollection/IMyDictionary.cs
@@ -4,9 +4,11 @@ using System.Text;
 
 namespace IntechCode.IntechCollection
 {
-    public interface IMyDictionary<TKey,TValue>
+    public interface IMyDictionary<TKey,TValue> : IMyEnumerable<KeyValuePair<TKey,TValue>>
     {
-
+        /// <summary>
+        /// Gets the count of key-value pairs that exist in this dictionary.
+        /// </summary>
         int Count { get; }
 
         /// <summary>
@@ -16,8 +18,18 @@ namespace IntechCode.IntechCollection
         /// <param name="value">The value to add.</param>
         void Add(TKey key, TValue value);
 
+        /// <summary>
+        /// Removes a key.
+        /// </summary>
+        /// <param name="key">The key to remove.</param>
+        /// <returns>True on success, false if the key did not exist.</returns>
         bool Remove(TKey key);
 
+        /// <summary>
+        /// Tests whether a key exists in the dictionary.
+        /// </summary>
+        /// <param name="key">The key to challenge.</param>
+        /// <returns>True if the key exists, false otherwise.</returns>
         bool ContainsKey(TKey key);
 
         /// <summary>
