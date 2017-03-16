2a9d32564b8ab9c871d189f77d8ea7e21ea9832a
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-03-16 12:54:24 +0100

    IMyDictionary interface (to be implemented).

--- FirstSolution/IntechCode/IntechCollection/IMyDictionary.cs
+++ FirstSolution/IntechCode/IntechCollection/IMyDictionary.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace IntechCode.IntechCollection
 {
-    public interface IMyDictionary
+    public interface IMyDictionary<TKey,TValue>
     {
+
+        int Count { get; }
+
+        /// <summary>
+        /// Adds a key-value pair. The key must not already exist.
+        /// </summary>
+        /// <param name="key">The key to add.</param>
+        /// <param name="value">The value to add.</param>
+        void Add(TKey key, TValue value);
+
+        bool Remove(TKey key);
+
+        bool ContainsKey(TKey key);
+
+        /// <summary>
+        /// Gets the value associated to a key.
+        /// When the key DOES NOT EXIST: <see cref="KeyNotFoundException"/>.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        TValue this[TKey key] { get; }
+
+
+        bool TryGetValue(TKey key, out TValue value);
+
     }
 }
---
+++ FirstSolution/Tests/IntechCode.Tests/ByRefOrOutTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntechCode.Tests
+{
+    public class ByRefOrOutTests
+    {
+        [Fact]
+        public void byref_demo()
+        {
+            int i = 0;
+            IChangeMyParam(ref i);
+            i.Should().Be(9786);
+            IInitializeMyParam(out i);
+            int j;
+            IInitializeMyParam(out j)
+;        }
+
+        static void IChangeMyParam(ref int param) => param += 9786;
+        static void IInitializeMyParam(out int param) => param = 123;
+    }
+}
