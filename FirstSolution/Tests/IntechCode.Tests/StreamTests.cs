595c6fad19d4604e8bed3a24d4253c7687dda426
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-04-07 11:03:20 +0200

    Starting Streams.

---
+++ FirstSolution/Tests/IntechCode.Tests/StreamTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntechCode.Tests
+{
+    [TestFixture]
+    public class StreamTests
+    {
+        const string demoPath = @"C:\Intech\2017-1\S7-8\2017-1-IL-A4\FirstSolution\Tests\IntechCode.Tests\StreamTests.cs";
+
+        [Test]
+        public void copying_a_stream()
+        {
+
+        }
+
+        static void CopyFromTo( Stream input, Stream output )
+        {
+
+        }
+    }
+}
