2fb9c6e5c6cc856a906f8122b5e23bc6ea9e2f26
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-04-07 11:33:23 +0200

    Do it

--- FirstSolution/Tests/IntechCode.Tests/StreamTests.cs
+++ FirstSolution/Tests/IntechCode.Tests/StreamTests.cs
@@ -12,10 +12,16 @@ namespace IntechCode.Tests
     public class StreamTests
     {
         const string demoPath = @"C:\Intech\2017-1\S7-8\2017-1-IL-A4\FirstSolution\Tests\IntechCode.Tests\StreamTests.cs";
+        const string outputPath = demoPath + ".bak";
 
         [Test]
         public void copying_a_stream()
         {
+            using (FileStream input = new FileStream(demoPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (Stream output = new FileStream(outputPath, FileMode.Create, FileAccess.Write, FileShare.Read))
+            {
+                CopyFromTo(input, output);
+            }
 
         }
 
